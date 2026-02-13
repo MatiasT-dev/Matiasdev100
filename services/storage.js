1063ce77da0ebeaec9345c70b895a5ce4c44a392
MatiasT-dev <158021604+MatiasT-dev@users.noreply.github.com>
2026-02-13 10:43:45 +0200

    Update storage.js

--- services/storage.js
+++ services/storage.js
@@ -1,1 +1,33 @@
+ let piste = 0;
 
+
+//pisten päivityksen
+        function paivitapiste() {
+            document.getElementById("piste").innerHTML = piste;
+        }
+
+function lisaapiste() {
+        piste++;
+  paivitapiste();
+
+}
+ //pisteen vähetäminen
+        function pienenapiste() {
+            piste--;
+            paivitapiste();
+        }
+
+    function talepistee() {
+            localStorage.setItem("piste", piste);
+        
+        }
+
+//pisteiden uudelen ladatumiseen viimeksi talentuksesta
+        function uudelenpistee() {
+            let tale = localStorage.getItem("piste");
+            if (tale !== null) {
+            piste = Number(tale);
+            }
+            paivitapiste();
+        
+        }
