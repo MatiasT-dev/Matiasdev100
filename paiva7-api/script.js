b26c72b70ceaa2ee48b0cddd02417262d361c030
MatiasT-dev <158021604+MatiasT-dev@users.noreply.github.com>
2026-02-18 13:40:29 +0200

    Update script.js

--- paiva7-api/script.js
+++ paiva7-api/script.js
@@ -31,9 +31,164 @@ function naytaTulos(teksti) {
 
 
 
+function lisaaUser() {
+
+  asetaTila("Lähetetään...");
+
+ 
+
+  const uusi = {
+
+    name: "Testi Käyttäjä",
+
+    email: "testi@example.com"
+
+  };
+
+ 
+
+  fetch(API, {
+
+    method: "POST",
+
+    headers: { "Content-Type": "application/json" },
+
+    body: JSON.stringify(uusi)
+
+  })
+
+    .then(res => res.json())
+
+    .then(data => {
+
+      asetaTila("Lisäys onnistui");
+
+      nayta(data);
+
+    })
+
+    .catch(() => {
+
+      asetaTila("Lisäys epäonnistui");
+
+    });
+
+}
 
 
+function muokkaaUser() {
 
+  asetaTila("Muokataan...");
+
+ 
+
+  fetch(API + "/1", {
+
+    method: "PUT",
+
+    headers: { "Content-Type": "application/json" },
+
+    body: JSON.stringify({
+
+      id: 1,
+
+      name: "Muokattu Nimi",
+
+      email: "muokattu@example.com"
+
+    })
+
+  })
+
+    .then(res => res.json())
+
+    .then(data => {
+
+      asetaTila("Muokkaus onnistui");
+
+      nayta(data);
+
+    })
+
+    .catch(() => {
+
+      asetaTila("Muokkaus epäonnistui");
+
+    });
+
+}
+
+function poistaUser() {
+
+  asetaTila("Poistetaan...");
+
+ 
+
+  fetch(API + "/1", { method: "DELETE" })
+
+    .then(res => {
+
+      if (res.ok) {
+
+        asetaTila("Poisto onnistui");
+
+        nayta({ viesti: "Käyttäjä poistettu (harjoitus)" });
+
+      } else {
+
+        asetaTila("Poisto epäonnistui");
+
+      }
+
+    })
+
+    .catch(() => {
+
+      asetaTila("Poisto epäonnistui");
+
+    });
+
+}
+
+function naytaVainNimet() {
+
+  if (viimeisinData.length === 0) {
+
+    naytaTulos("Hae data ensin");
+
+    return;
+
+  }
+
+ 
+
+  const nimet = viimeisinData.map(item => item.name);
+
+  naytaTulos("Nimet: " + nimet.join(", "));
+
+}
+
+function suodataNimella() {
+
+  const tulos = viimeisinData.filter(item =>
+
+    item.name.toLowerCase().includes("a")
+
+  );
+
+ 
+
+  naytaTulos("Nimi sisältää 'a': " + tulos.length + " kpl");
+
+}
+
+function laskeMaara() {
+
+  const maara = viimeisinData.reduce(sum => sum + 1, 0);
+
+  naytaTulos("Käyttäjiä yhteensä: " + maara);
+
+}
 
 
 
